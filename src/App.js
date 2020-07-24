5540360c86b896a46c977146a69f3286b3f781ed
Mason Davis <masondavis2009@gmail.com>
2020-07-24 16:22:41 -0500

    ALL MVP MET

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from "react-router-dom";
 import Pizza from "./components/Pizza";
 import PizzaList from "./components/PizzaList";
 
+
 const App = () => {
   // const [nameState, setNameState] = useState({
   //   name: "",
@@ -14,10 +15,12 @@ const App = () => {
 
   return (
     <div className="App">
+      <img src="https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&w=1000&q=80" alt="Pizza"></img>
       <h1>Lambda City Pizza</h1>
-      <p>The best pizza delivery in the entire Northeast Lambda City region!</p>
+      <p>The best pizza delivery in the entire Northeast Lambda City region! Get the best pizza here.</p>
       <div className="nav-links">
-        <Link to="/">Pizza</Link>
+        <Link to="/">Home</Link>
+        <p>or</p>
         <Link to="/pizza-list">Build Your Pizza!</Link>
       </div>
       <Route exact path="/pizza-list/" component={PizzaList} />
